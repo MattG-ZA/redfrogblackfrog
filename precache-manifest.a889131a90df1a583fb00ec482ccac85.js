2efffba1106d992f08091486c01dbb18350a0889
MattG-ZA <gleedmatt@gmail.com>
2019-02-26 21:40:06 +0200

    Updates

---
+++ precache-manifest.a889131a90df1a583fb00ec482ccac85.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a9f245094e57b7d442ef",
+    "url": "/MattG-ZA/redfrogblackfrog/static/js/runtime~main.a9f24509.js"
+  },
+  {
+    "revision": "893154412df2d0117fc4",
+    "url": "/MattG-ZA/redfrogblackfrog/static/js/main.89315441.chunk.js"
+  },
+  {
+    "revision": "77958300264c6869a197",
+    "url": "/MattG-ZA/redfrogblackfrog/static/js/1.77958300.chunk.js"
+  },
+  {
+    "revision": "893154412df2d0117fc4",
+    "url": "/MattG-ZA/redfrogblackfrog/static/css/main.77d6319f.chunk.css"
+  },
+  {
+    "revision": "47af538cb937848c7fc747e967495eb2",
+    "url": "/MattG-ZA/redfrogblackfrog/index.html"
+  }
+];
